d47b35326f10cb77925e30826fb78642b2e4e3da
Lei <ratrateroo@gmail.com>
2021-01-27 02:50:06 +0800

    added logic to product page

--- ionic-react-mobile-app-1/src/pages/Product.tsx
+++ ionic-react-mobile-app-1/src/pages/Product.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
 	IonToolbar,
@@ -10,12 +10,27 @@ import {
 	IonCol,
 	IonButton,
 	IonIcon,
+	IonImg,
 } from '@ionic/react';
 import Head2 from '../components/head/Head2';
 import { star } from 'ionicons/icons';
 import './Product.css';
 
 const Product: React.FC = () => {
+	const [imageSelected, setImageSelected] = useState(
+		'assets/products/product11-1.jpg'
+	);
+
+	const [colorname, setColorName] = useState('Black');
+
+	const imageSelectHandler = (image: string) => {
+		setImageSelected(image);
+	};
+
+	const colorHandler = (color: string, image: string) => {
+		setColorName(color);
+		setImageSelected(image);
+	};
 	return (
 		<React.Fragment>
 			<Head2 />
@@ -87,7 +102,118 @@ const Product: React.FC = () => {
 						</IonCol>
 					</IonRow>
 				</IonGrid>
-				<IonGrid></IonGrid>
+				<IonGrid>
+					<IonRow>
+						<IonCol size="2">
+							<IonImg
+								onClick={() => {
+									imageSelectHandler(
+										'assets/products/product11-1.jpg'
+									);
+								}}
+								className="product-image-holder"
+								src="assets/products/product11-1.jpg"
+							/>
+							<IonImg
+								onClick={() => {
+									imageSelectHandler(
+										'assets/products/product11-2.jpg'
+									);
+								}}
+								className="product-image-holder"
+								src="assets/products/product11-2.jpg"
+							/>
+							<IonImg
+								onClick={() => {
+									imageSelectHandler(
+										'assets/products/product11-3.jpg'
+									);
+								}}
+								className="product-image-holder"
+								src="assets/products/product11-3.jpg"
+							/>
+							<IonImg
+								onClick={() => {
+									imageSelectHandler(
+										'assets/products/product11-4.jpg'
+									);
+								}}
+								className="product-image-holder"
+								src="assets/products/product11-4.jpg"
+							/>
+						</IonCol>
+						<IonCol size="10">
+							<IonImg
+								className="product-image-holder"
+								src={imageSelected}
+							/>
+						</IonCol>
+					</IonRow>
+				</IonGrid>
+				<IonGrid>
+					<IonRow>
+						<IonCol size="3">
+							<IonText color="darkpurple">
+								<h5>Color: {colorname}</h5>
+							</IonText>
+						</IonCol>
+
+						<IonCol>
+							<IonGrid>
+								<IonRow>
+									<IonCol>
+										<IonImg
+											onClick={() => {
+												colorHandler(
+													'Yellow',
+													'assets/products/product11-y.jpg'
+												);
+											}}
+											className="product-image-holder-mini"
+											src="assets/products/product11-y.jpg"
+										/>
+									</IonCol>
+									<IonCol>
+										<IonImg
+											onClick={() => {
+												colorHandler(
+													'Blue',
+													'assets/products/product11-b.jpg'
+												);
+											}}
+											className="product-image-holder-mini"
+											src="assets/products/product11-b.jpg"
+										/>
+									</IonCol>
+									<IonCol>
+										<IonImg
+											onClick={() => {
+												colorHandler(
+													'Silver',
+													'assets/products/product11-s.jpg'
+												);
+											}}
+											className="product-image-holder-mini"
+											src="assets/products/product11-s.jpg"
+										/>
+									</IonCol>
+									<IonCol>
+										<IonImg
+											onClick={() => {
+												colorHandler(
+													'Black',
+													'assets/products/product11-1.jpg'
+												);
+											}}
+											className="product-image-holder-mini"
+											src="assets/products/product11-1.jpg"
+										/>
+									</IonCol>
+								</IonRow>
+							</IonGrid>
+						</IonCol>
+					</IonRow>
+				</IonGrid>
 			</IonContent>
 		</React.Fragment>
 	);
