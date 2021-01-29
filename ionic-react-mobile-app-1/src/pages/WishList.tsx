dea6344818b4466326c40a6bd5fa6c7e9220b0ba
Lei <ratrateroo@gmail.com>
2021-01-29 17:01:55 +0800

    updated wishlist page

--- ionic-react-mobile-app-1/src/pages/WishList.tsx
+++ ionic-react-mobile-app-1/src/pages/WishList.tsx
@@ -11,9 +11,10 @@ import {
 	IonLabel,
 	IonInput,
 	IonIcon,
+	IonImg,
 } from '@ionic/react';
 import Head2 from '../components/head/Head2';
-import { cart } from 'ionicons/icons';
+import { caretDownOutline, cart } from 'ionicons/icons';
 import './WishList.css';
 const WishList: React.FC = () => {
 	return (
@@ -36,9 +37,50 @@ const WishList: React.FC = () => {
 										slot="icon-only"
 										size="large"
 									/>
-									<sup>0</sup>
+									<span className="cart-item-number">0</span>
 								</h5>
 							</IonText>
+						</IonCol>
+					</IonRow>
+				</IonGrid>
+				<IonGrid>
+					<div className="wishlist-item-card">
+						<IonRow>
+							<IonCol size="4">
+								<IonImg
+									className="withlist-image"
+									src="assets/products/product1.jpg"
+								/>
+							</IonCol>
+							<IonCol size="4">
+								<IonText className="" color="darkpurple">
+									<p>Evil Eye Necklace</p>
+								</IonText>
+								<IonText className="" color="orange">
+									<p>Swarovski</p>
+								</IonText>
+								<IonText className="" color="coal">
+									<p>$ 165.37</p>
+								</IonText>
+							</IonCol>
+							<IonCol size="4">
+								<IonButton color="orange" expand="block" size="small">
+									Add to Cart
+								</IonButton>
+								<IonButton color="red" expand="block" size="small">
+									Delete
+								</IonButton>
+							</IonCol>
+						</IonRow>
+					</div>
+				</IonGrid>
+
+				<IonGrid>
+					<IonRow>
+						<IonCol>
+							<IonButton color="darkpurple" expand="full">
+								Continue Shopping
+							</IonButton>
 						</IonCol>
 					</IonRow>
 				</IonGrid>
